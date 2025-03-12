0ba0c444de48b2b19659fdb610e9dc2e5ac1c528
Thanasis <thanasis.trantas@tno.nl>
2025-03-12 10:31:39 +0100

    tests done

--- tests/test_basic_aurora.py
+++ tests/test_basic_aurora.py
@@ -2,28 +2,44 @@ from datetime import datetime
 
 import torch
 
-from aurora import AuroraSmall, Batch, Metadata
+from aurora import AuroraSmall, Batch, Metadata, rollout
 
-def test_load_and_random_prediction():
-    model = AuroraSmall()
-
-    model.load_checkpoint("microsoft/aurora", "aurora-0.25-small-pretrained.ckpt")
-
-    batch = Batch(
-        surf_vars={k: torch.randn(1, 2, 17, 32) for k in ("2t", "10u", "10v", "msl")},
-        static_vars={k: torch.randn(17, 32) for k in ("lsm", "z", "slt")},
-        atmos_vars={k: torch.randn(1, 2, 4, 17, 32) for k in ("z", "u", "v", "t", "q")},
-        metadata=Metadata(
-            lat=torch.linspace(90, -90, 17),
-            lon=torch.linspace(0, 360, 32 + 1)[:-1],
-            time=(datetime(2020, 6, 1, 12, 0),),
-            atmos_levels=(100, 250, 500, 850),
-        ),
-    )
-
+def test_load_and_random_prediction(model):
     prediction = model.forward(batch)
 
     print(prediction.surf_vars["2t"])
 
+def prediction_aurora(model):
+    model.eval()
+
+    with torch.inference_mode():
+        pred = model.forward(batch)
+        print(pred)
+
+def rollout_aurora(model):
+    with torch.inference_mode():
+        preds = [pred.to("cpu") for pred in rollout(model, batch, steps=2)]
+        print(preds)
+
 if __name__ == "__main__":
-    test_load_and_random_prediction()
+
+    batch = Batch(
+    surf_vars={k: torch.randn(1, 2, 17, 32) for k in ("2t", "10u", "10v", "msl")},
+    static_vars={k: torch.randn(17, 32) for k in ("lsm", "z", "slt")},
+    atmos_vars={k: torch.randn(1, 2, 4, 17, 32) for k in ("z", "u", "v", "t", "q")},
+    metadata=Metadata(
+        lat=torch.linspace(90, -90, 17),
+        lon=torch.linspace(0, 360, 32 + 1)[:-1],
+        time=(datetime(2020, 6, 1, 12, 0),),
+        atmos_levels=(100, 250, 500, 850),
+    ),
+    )
+
+    model = AuroraSmall()
+
+    model.load_checkpoint("microsoft/aurora", "aurora-0.25-small-pretrained.ckpt")
+    model.to("cuda")
+
+    test_load_and_random_prediction(model)
+    prediction_aurora(model)
+    rollout_aurora(model)
