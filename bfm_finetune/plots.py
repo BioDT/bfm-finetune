d528a44422b1250644a6bd82e0b847f7076d7d39
Martino Mensio <martino.mensio@tno.nl>
2025-03-19 16:44:29 +0100

    plot species in notebook

--- bfm_finetune/plots.py
+++ bfm_finetune/plots.py
@@ -1,30 +1,18 @@
+import math
+from typing import List
+
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
-
-
-def plot_matrix(lat_range, lon_range, matrix: np.ndarray):
-    assert matrix.shape[0] == len(
-        lat_range
-    ), f"lat_range and matrix.shape[0] mismatch: {len(lat_range)}, {matrix.shape[0]}"
-    assert matrix.shape[1] == len(
-        lon_range
-    ), f"lon_range and matrix.shape[1] mismatch: {len(lon_range)}, {matrix.shape[1]}"
-    unrolled = []
-    for lat_i, lat_val in enumerate(lat_range):
-        for lon_i, lon_val in enumerate(lon_range):
-            unrolled.append(
-                {"lat": lat_val, "lon": lon_val, "value": matrix[lat_i, lon_i]}
-            )
-    df = pd.DataFrame(unrolled)
-    return plot_df_latlon(df)
+from plotly.subplots import make_subplots
 
 
 def plot_df_latlon(
     df: pd.DataFrame,
+    title: str,
     lat_key: str = "lat",
     lon_key: str = "lon",
-    value_key: str = "value",
+    value_keys: List[str] = ["value"],
     radius: int = 10,
 ):
     min_lat = df[lat_key].min()
@@ -33,22 +21,66 @@ def plot_df_latlon(
     max_lon = df[lon_key].max()
     print("lat", min_lat, max_lat)
     print("lon", min_lon, max_lon)
-    fig = go.Figure(
-        go.Densitymap(lat=df[lat_key], lon=df[lon_key], z=df[value_key], radius=radius)
+    # fig = go.Figure()
+    columns = math.ceil(math.sqrt(len(value_keys)))
+    rows = math.ceil(len(value_keys) / columns)
+    fig = make_subplots(
+        rows=rows,
+        cols=columns,
+        subplot_titles=value_keys,
+        specs=[[{"type": "scattergeo"}] * columns] * rows,
     )
-    fig.update_layout(
-        map_style="open-street-map",
-        map_center_lon=(min_lon + max_lon) / 2,
-        map_center_lat=(min_lat + max_lat) / 2,
-        map_bounds_north=max_lat,
-        map_bounds_south=min_lat,
-        map_bounds_east=max_lon,
-        map_bounds_west=min_lon,
+    max_value = max(df[value_keys].max().tolist())
+    for i, value_key in enumerate(value_keys):
+        df_filtered = df[df[value_key] > 0.0]
+        fig.add_trace(
+            # go.Densitymap(
+            #     lat=df[lat_key],
+            #     lon=df[lon_key],
+            #     z=df[value_key],
+            #     radius=radius,
+            #     name=value_key,
+            # )
+            go.Scattergeo(
+                lat=df_filtered[lat_key],
+                lon=df_filtered[lon_key],
+                text=df_filtered[value_key],
+                # marker_color=df_filtered[value_key],
+                # radius=radius,
+                marker=dict(
+                    size=8,
+                    opacity=df_filtered[value_key] / max_value,
+                ),
+                name=value_key,
+            ),
+            col=i % columns + 1,
+            row=math.floor(i / columns) + 1,
+        )
+    # for c in range(columns)
+    # for c in range(columns)
+    geo = dict(
+        scope="europe",
+        resolution=50,
+        lonaxis=dict(range=[min_lon, max_lon]),
+        lataxis=dict(range=[min_lat, max_lat]),
     )
-    fig.update_layout(
-        margin={"r": 0, "t": 0, "l": 0, "b": 0},
-        # width=(max_lon - min_lon) * 20,
-        # height=(max_lat - min_lat) * 20 + 400,
-    )
+    geo_dict = {f"geo{i+1}": geo for i in range(len(value_keys))}
+    # fig.update_layout(  # TODO: only works for the first subplot???
+    #     #     # map_style="open-street-map",
+    #     #     map_center_lon=(min_lon + max_lon) / 2,
+    #     #     map_center_lat=(min_lat + max_lat) / 2,
+    #     #     map_bounds_north=max_lat,
+    #     #     map_bounds_south=min_lat,
+    #     #     map_bounds_east=max_lon,
+    #     #     map_bounds_west=min_lon,
+    #     # geo=geo,
+    # )
+    fig.update_layout(geo_dict)
+    fig.update_layout(title=title)
+    # fig.update_layout(
+    #     margin={"r": 0, "t": 0, "l": 0, "b": 0},
+    #     # width=(max_lon - min_lon) * 20,
+    #     # height=(max_lat - min_lat) * 20 + 400,
+    # )
     # fig.show()
     return fig
--- bfm_finetune/utils.py
+++ bfm_finetune/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+from tqdm import tqdm
 
 
 def get_lat_lon_ranges(
@@ -28,3 +30,50 @@ def get_lat_lon_ranges(
     lat_range = lat_range[::-1]
 
     return lat_range, lon_range
+
+
+def aggregate_into_latlon_grid(
+    df: pd.DataFrame, lat_range: np.ndarray, lon_range: np.ndarray, step: float
+) -> np.ndarray:
+    matrix = np.zeros((len(lat_range), len(lon_range)))
+    # slower loop
+    # for lat_i, lat_val in enumerate(tqdm(lat_range)):
+    #     for lon_i, lon_val in enumerate(lon_range):
+    #         df_here = df[
+    #             (df["lat"] >= lat_val - step / 2)
+    #             & (df["lat"] < lat_val + step / 2)
+    #             & (df["lon"] >= lon_val - step / 2)
+    #             & (df["lon"] < lon_val + step / 2)
+    #         ]
+    #         # print(len(df_here))
+    #         matrix[lat_i, lon_i] = len(df_here)
+    lat_range_list = lat_range.tolist()
+    lon_range_list = lon_range.tolist()
+    # faster loop
+    for index, row in tqdm(df.iterrows()):
+        lat = row["lat"]
+        lon = row["lon"]
+        lat_i = next(i for i, val in enumerate(lat_range_list) if val <= lat + step / 2)
+        lon_i = next(i for i, val in enumerate(lon_range_list) if val >= lon - step / 2)
+        # print(lat_i, lat, lat_range)
+        # raise ValueError(123)
+        matrix[lat_i, lon_i] += 1.0
+
+    return matrix
+
+
+def unroll_matrix_into_df(lat_range, lon_range, matrix: np.ndarray):
+    assert matrix.shape[0] == len(
+        lat_range
+    ), f"lat_range and matrix.shape[0] mismatch: {len(lat_range)}, {matrix.shape[0]}"
+    assert matrix.shape[1] == len(
+        lon_range
+    ), f"lon_range and matrix.shape[1] mismatch: {len(lon_range)}, {matrix.shape[1]}"
+    unrolled = []
+    for lat_i, lat_val in enumerate(lat_range):
+        for lon_i, lon_val in enumerate(lon_range):
+            unrolled.append(
+                {"lat": lat_val, "lon": lon_val, "value": matrix[lat_i, lon_i]}
+            )
+    df = pd.DataFrame(unrolled)
+    return df
